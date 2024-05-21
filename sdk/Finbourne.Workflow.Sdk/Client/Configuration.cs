f2947e2aef350dd7478fce149e7021b11bee18b0
Concourse <concourse@finbourne.com>
2024-05-21 21:03:55 +0000

    Update SDK 0.1.1044

--- sdk/Finbourne.Workflow.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Workflow.Sdk/Client/Configuration.cs
@@ -29,7 +29,7 @@ namespace Finbourne.Workflow.Sdk.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.1.1043";
+        public const string Version = "0.1.1044";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -111,7 +111,7 @@ namespace Finbourne.Workflow.Sdk.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.1043/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/0.1.1044/csharp");
             BasePath = "https://www.lusid.com/workflow";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -518,8 +518,8 @@ namespace Finbourne.Workflow.Sdk.Client
             string report = "C# SDK (Finbourne.Workflow.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.1043\n";
-            report += "    SDK Package Version: 0.1.1043\n";
+            report += "    Version of the API: 0.1.1044\n";
+            report += "    SDK Package Version: 0.1.1044\n";
 
             return report;
         }
